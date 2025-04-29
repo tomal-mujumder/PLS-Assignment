1f124874ff092ac0e27f25f3ca9fb3cf32afe319
Tomal Mujumder <129778988+tomal-mujumder@users.noreply.github.com>
2025-04-29 10:37:46 +0600

    Add files via upload

---
+++ fixed_heap_dynamic.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size = 5;
+    int* arr = new int[size]; 
+    for (int i = 0; i < size; i++) arr[i] = i;
+    for (int i = 0; i < size; i++) cout << arr[i] << " ";
+    delete[] arr;
+    return 0;
+}
